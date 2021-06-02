6163b4b322fb830e81df4e3159d19aaefdce58ac
Fleming <47439078+Fleming@users.noreply.github.com>
2021-06-02 17:30:06 -0400

    changed

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # import necessary libraries
-from models import create_classes
+from flask_sqlalchemy import SQLAlchemy
 import os
-import joblib
 from flask import (
     Flask,
     render_template,
@@ -10,11 +9,11 @@ from flask import (
     redirect)
 
 
+import pandas
+from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
+import joblib
 
-
-#################################################
-# Flask Setup
-#################################################
 app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
@@ -25,18 +24,28 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-Home = create_classes(db)
+# Home = create_classes(db)
+# create route that renders index.html template
+
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
 # create route that renders index.html template
 
 
 @app.route("/")
-def home():
-    #return render_template("index.html")
+def welcome():
+    app.logger.info("home")
     return render_template("form.html", price= "$0.00", bed = "", bath = "", condition = "", age = "", sqft = "")
+   # return render_template("index.html")
 
 # Query the database and send the jsonified results
 @app.route("/send", methods=["GET", "POST"])
 def send():
+    app.logger.info("reached beginning of method")
+
     predicted_price_rounded = 0
     gotvalues=  False
 
@@ -61,11 +70,10 @@ def send():
             [[numBedRooms, numBathRooms, houseSq, condition_array[0], condition_array[1], condition_array[2], condition_array[3], houseAge]])
         predicted_price = loaded_y_scaler.inverse_transform(
             linear_ridge_model.predict(house_data_scaled))
-        predicted_price_rounded = "${:,.2f}".format(
-            round(predicted_price[0][0]))
-        return_predicted_price = [{
-            "predited_price": predicted_price_rounded
-        }]
+        predicted_price_rounded = "${:,.2f}".format(round(predicted_price[0][0]))
+      #  return_predicted_price = [{
+      #      "predited_price": predicted_price_rounded
+      #  }]
 
     bedReturn = numBedRooms if gotvalues else ""
     bathReturn = numBathRooms if gotvalues else ""
@@ -74,9 +82,9 @@ def send():
     sqftReturn = houseSq if gotvalues else ""
 
 
-    return render_template("form.html", price= predicted_price_rounded)
-
-
+    return render_template("form.html", price = predicted_price_rounded, bed = bedReturn, bath = bathReturn, condition = conditionReturn, age = ageReturn, sqft = sqftReturn)
+    # return render_template("form.html", price= "$0.00", bed = "", bath = "", condition = "", age = "", sqft = "")
+    # redirect("/", pricepredicted = return_predicted_price) 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=False)
